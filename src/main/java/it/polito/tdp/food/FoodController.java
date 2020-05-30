6b6f9be55040229fb8b6bfa0e789d2ad2a68fc07
marti <marti@DESKTOP-U4VTACU>
2020-05-30 17:25:59 +0200

    Svolgimento parte 1

--- src/main/java/it/polito/tdp/food/FoodController.java
+++ src/main/java/it/polito/tdp/food/FoodController.java
@@ -5,8 +5,12 @@
 package it.polito.tdp.food;
 
 import java.net.URL;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import it.polito.tdp.food.model.Food;
 import it.polito.tdp.food.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,66 +20,105 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FoodController {
-	
+
 	private Model model;
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
+	@FXML // ResourceBundle that was given to the FXMLLoader
+	private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+	@FXML // URL location of the FXML file that was given to the FXMLLoader
+	private URL location;
 
-    @FXML // fx:id="txtPorzioni"
-    private TextField txtPorzioni; // Value injected by FXMLLoader
+	@FXML // fx:id="txtPorzioni"
+	private TextField txtPorzioni; // Value injected by FXMLLoader
 
-    @FXML // fx:id="txtK"
-    private TextField txtK; // Value injected by FXMLLoader
+	@FXML // fx:id="txtK"
+	private TextField txtK; // Value injected by FXMLLoader
 
-    @FXML // fx:id="btnAnalisi"
-    private Button btnAnalisi; // Value injected by FXMLLoader
+	@FXML // fx:id="btnAnalisi"
+	private Button btnAnalisi; // Value injected by FXMLLoader
 
-    @FXML // fx:id="btnCalorie"
-    private Button btnCalorie; // Value injected by FXMLLoader
+	@FXML // fx:id="btnCalorie"
+	private Button btnCalorie; // Value injected by FXMLLoader
 
-    @FXML // fx:id="btnSimula"
-    private Button btnSimula; // Value injected by FXMLLoader
+	@FXML // fx:id="btnSimula"
+	private Button btnSimula; // Value injected by FXMLLoader
 
-    @FXML // fx:id="boxFood"
-    private ComboBox<?> boxFood; // Value injected by FXMLLoader
+	@FXML // fx:id="boxFood"
+	private ComboBox<Food> boxFood; // Value injected by FXMLLoader
 
-    @FXML // fx:id="txtResult"
-    private TextArea txtResult; // Value injected by FXMLLoader
+	@FXML // fx:id="txtResult"
+	private TextArea txtResult; // Value injected by FXMLLoader
 
-    @FXML
-    void doCreaGrafo(ActionEvent event) {
-    	txtResult.clear();
-    	txtResult.appendText("Creazione grafo...");
-    }
-    
-    @FXML
-    void doCalorie(ActionEvent event) {
-    	txtResult.clear();
-    	txtResult.appendText("Analisi calorie...");
-    }
+	@FXML
+	void doCreaGrafo(ActionEvent event) {
+		try {
+			int porzioni = Integer.parseInt(txtPorzioni.getText());
+			if (porzioni == 0) {
+				txtResult.setText("Numero porzioni non valido");
+				return;
+			}
+			model.creaGrafo(porzioni);
+			txtResult.setText(String.format("Grafo creato con %d vertici e %d archi \n", model.getNumVetex(), model.getNumEdges()));
+			
+			List<Food> listaCibi = new LinkedList<Food>(model.getMappaCibi().values());
+			listaCibi.sort(new Comparator<Food>() {
 
-    @FXML
-    void doSimula(ActionEvent event) {
-    	txtResult.clear();
-    	txtResult.appendText("Simulazione...");
-    }
+				@Override
+				public int compare(Food o1, Food o2) {
+					return o1.getDisplay_name().compareTo(o2.getDisplay_name());
+				}
+			});
+			
+			boxFood.getItems().addAll(listaCibi);
+			boxFood.setValue(listaCibi.get(0));
+			
+			boxFood.setDisable(false);
+			btnCalorie.setDisable(false);
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-        assert txtPorzioni != null : "fx:id=\"txtPorzioni\" was not injected: check your FXML file 'Food.fxml'.";
-        assert txtK != null : "fx:id=\"txtK\" was not injected: check your FXML file 'Food.fxml'.";
-        assert btnAnalisi != null : "fx:id=\"btnAnalisi\" was not injected: check your FXML file 'Food.fxml'.";
-        assert btnCalorie != null : "fx:id=\"btnCalorie\" was not injected: check your FXML file 'Food.fxml'.";
-        assert btnSimula != null : "fx:id=\"btnSimula\" was not injected: check your FXML file 'Food.fxml'.";
-        assert boxFood != null : "fx:id=\"boxFood\" was not injected: check your FXML file 'Food.fxml'.";
-        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Food.fxml'.";
-    }
-    
-    public void setModel(Model model) {
-    	this.model = model;
-    }
+		} catch (NumberFormatException e) {
+			txtResult.setText("Numero porzioni non valido");
+		} catch (Exception e) {
+			txtResult.setText("ERRORE!!!");
+		}
+
+	}
+
+	@FXML
+	void doCalorie(ActionEvent event) {
+		Food cibo = boxFood.getValue();
+		if(model.best(cibo)==null) {
+			txtResult.appendText("Non hai almeno 5 ingredienti per questo cibo \n");
+			return;
+		}
+		txtResult.appendText(String.format("Per il cibo %s i 5 ingredienti più calori sono: \n", cibo.getDisplay_name()));
+		for(Food f: model.best(cibo)) {
+			txtResult.appendText(f+"\n");
+		}
+	}
+
+	@FXML
+	void doSimula(ActionEvent event) {
+		txtResult.clear();
+		txtResult.appendText("Simulazione...");
+	}
+
+	@FXML // This method is called by the FXMLLoader when initialization is complete
+	void initialize() {
+		assert txtPorzioni != null : "fx:id=\"txtPorzioni\" was not injected: check your FXML file 'Food.fxml'.";
+		assert txtK != null : "fx:id=\"txtK\" was not injected: check your FXML file 'Food.fxml'.";
+		assert btnAnalisi != null : "fx:id=\"btnAnalisi\" was not injected: check your FXML file 'Food.fxml'.";
+		assert btnCalorie != null : "fx:id=\"btnCalorie\" was not injected: check your FXML file 'Food.fxml'.";
+		assert btnSimula != null : "fx:id=\"btnSimula\" was not injected: check your FXML file 'Food.fxml'.";
+		assert boxFood != null : "fx:id=\"boxFood\" was not injected: check your FXML file 'Food.fxml'.";
+		assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Food.fxml'.";
+		
+		boxFood.setDisable(true);
+		btnCalorie.setDisable(true);
+		btnSimula.setDisable(true);
+	}
+
+	public void setModel(Model model) {
+		this.model = model;
+	}
 }
--- src/main/java/it/polito/tdp/food/model/Model.java
+++ src/main/java/it/polito/tdp/food/model/Model.java
@@ -1,5 +1,74 @@
 package it.polito.tdp.food.model;
 
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.food.db.FoodDao;
+
 public class Model {
+
+	private Map<Integer, Food> mappaCibi;
+	private FoodDao dao;
+	private Graph<Food, DefaultWeightedEdge> grafo;
+
+	public Model() {
+		dao = new FoodDao();
+	}
+
+	public Map<Integer, Food> getMappaCibi() {
+		return mappaCibi;
+	}
+
+	public void creaGrafo(int porzioni) {
+		mappaCibi = new TreeMap<Integer, Food>();
+		dao.getCibiPerPorzione(mappaCibi, porzioni);
+		grafo = new SimpleWeightedGraph<Food, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+
+		// VERTICI
+		Graphs.addAllVertices(grafo, mappaCibi.values());
+		
+		//ARCHI
+		for(Coppie c: dao.listAllCoppie(mappaCibi)) {
+			Graphs.addEdge(grafo, c.getF1(), c.getF2(), c.getPeso());
+		}
+
+	}
+
+	public int getNumVetex() {
+		return grafo.vertexSet().size();
+	}
+
+	public int getNumEdges() {
+		return grafo.edgeSet().size();
+	}
+	
+	public List<Food> best(Food cibo){
+		List<DefaultWeightedEdge> archi = new LinkedList<DefaultWeightedEdge>(grafo.edgesOf(cibo));
+		archi.sort(new Comparator<DefaultWeightedEdge>() {
+
+			@Override
+			public int compare(DefaultWeightedEdge o1, DefaultWeightedEdge o2) {
+				return (int) (grafo.getEdgeWeight(o1)-grafo.getEdgeWeight(o2));
+			}
+		});
+		
+		List<Food> best = new LinkedList<Food>();
+		if(archi.size()<5) {
+			return null;
+		}
+		for(int i=0; i<5; i++) {
+			best.add(grafo.getEdgeTarget(archi.get(i)));
+		}
+		
+		return best;
+	}
 
 }
